aee784467a2a805ccc461dc3dc53713cdea381de
Kevin Harrington <mad.hephaestus@gmail.com>
2023-05-19 12:42:01 -0400

    adding code

--- YoloJava.groovy
+++ YoloJava.groovy
@@ -1,5 +1,27 @@
-implementation "ai.djl:api:0.4.0"
-    implementation "ai.djl:repository:0.4.0"
-    runtimeOnly "ai.djl.pytorch:pytorch-model-zoo:0.4.0"
-    runtimeOnly "ai.djl.pytorch:pytorch-native-auto:1.4.0"
-    
+@Grab(group='ai.djl', module='api', version='0.4.0')
+@Grab(group='ai.djl', module='repository', version='0.4.0')
+@Grab(group='ai.djl.pytorch', module='pytorch-model-zoo', version='0.4.0')
+@Grab(group='ai.djl.pytorch', module='pytorch-native-auto', version='0.4.0')
+
+
+
+
+
+    String url = "https://github.com/awslabs/djl/raw/master/examples/src/test/resources/dog_bike_car.jpg";
+    BufferedImage img = BufferedImageUtils.fromUrl(url);
+
+    Criteria<BufferedImage, DetectedObjects> criteria =
+            Criteria.builder()
+                    .optApplication(Application.CV.OBJECT_DETECTION)
+                    .setTypes(BufferedImage.class, DetectedObjects.class)
+                    .optFilter("backbone", "resnet50")
+                    .optProgress(new ProgressBar())
+                    .build();
+
+    try (ZooModel<BufferedImage, DetectedObjects> model = ModelZoo.loadModel(criteria)) {
+        try (Predictor<BufferedImage, DetectedObjects> predictor = model.newPredictor()) {
+            DetectedObjects detection = predictor.predict(img);
+            System.out.println(detection);
+        }
+    }
+println "Starting Yolo PyTorch Java example"
