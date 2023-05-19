17b6a48ec820c75f7301b0f07ec1e9406a836d21
Kevin Harrington <mad.hephaestus@gmail.com>
2023-05-19 12:32:28 -0400

    imports

--- YoloJava.groovy
+++ YoloJava.groovy
@@ -1,1 +1,5 @@
-// code here
+implementation "ai.djl:api:0.4.0"
+    implementation "ai.djl:repository:0.4.0"
+    runtimeOnly "ai.djl.pytorch:pytorch-model-zoo:0.4.0"
+    runtimeOnly "ai.djl.pytorch:pytorch-native-auto:1.4.0"
+    
