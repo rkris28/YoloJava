cbdbd3c40cbf98419f1c6bb22215ceca79af4475
Kevin Harrington <mad.hephaestus@gmail.com>
2023-05-25 17:45:10 -0400

    adding resilliancy

--- ResnetWithCamera.groovy
+++ ResnetWithCamera.groovy
@@ -10,7 +10,7 @@ import ai.djl.modality.cv.output.Landmark
 import ai.djl.repository.zoo.ZooModel
 
 @Grab(group='org.openpnp', module='opencv', version='4.7.0-0')
-
+//@Grab(group='org.tensorflow', module='tensorflow', version='1.15.0')
 
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
@@ -28,14 +28,18 @@ import java.awt.image.WritableRaster;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
-
+import java.nio.file.Files
+import java.nio.file.Path
 import javafx.application.Application;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Label
 import javafx.scene.control.Tab
 import javafx.scene.image.ImageView;
 import javafx.scene.image.WritableImage;
+import javafx.scene.layout.HBox
+import javafx.scene.layout.VBox
 
 import org.opencv.videoio.VideoCapture;
 
@@ -55,6 +59,12 @@ import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
 import org.opencv.objdetect.Objdetect;
 import org.opencv.videoio.VideoCapture;
+//import org.tensorflow.Graph;
+//import org.tensorflow.Session;
+//import org.tensorflow.Tensor;
+//import org.tensorflow.TensorFlow;
+
+
 // For proper execution of native libraries
 // Core.NATIVE_LIBRARY_NAME must be loaded before
 // calling any of the opencv methods
@@ -64,6 +74,118 @@ try {
 	BowlerStudio.printStackTrace(t)
 	return
 }
+//Map<String, float[]> faceDb = new HashMap<>();
+//FaceNetModel faceNetModel = new FaceNetModel("facenet.pb");
+//class FaceNetModel {
+//
+//	private Graph graph;
+//	private Session session;
+//	private String modelPath;
+//
+//	public FaceNetModel(String modelPath) {
+//		this.modelPath = modelPath;
+//	}
+//
+//	public void loadModel() {
+//		graph = new Graph();
+//		byte[] modelBytes = readAllBytesOrExit(Paths.get(modelPath));
+//		graph.importGraphDef(modelBytes);
+//		session = new Session(graph);
+//	}
+//
+//	public float[] getFaceEmbedding(Mat face) {
+//		float[] embedding = null;
+//		try {
+//			Tensor<Float> tensor = normalizeImage(face)
+//			Tensor<Float> output = session.runner()
+//			.feed("input_1", tensor)
+//			.fetch("Bottleneck_BatchNorm/batchnorm/add_1")
+//			.run()
+//			.get(0)
+//			.expect(Float.class);
+//			embedding = new float[(int) output.shape()[1]];
+//			output.copyTo(embedding);
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		return embedding;
+//	}
+//
+//	private Tensor<Float> normalizeImage(Mat mat) {
+//		Imgproc.cvtColor(mat, mat, Imgproc.COLOR_BGR2RGB);
+//		mat.convertTo(mat, CvType.CV_32F);
+//		Core.divide(mat, Scalar.all(255.0f), mat);
+//		return Tensor.create(mat.reshape(1, 160, 160, 3));
+//	}
+//
+//	private static byte[] readAllBytesOrExit(Path path) {
+//		try {
+//			return Files.readAllBytes(path);
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//			System.exit(-1);
+//		}
+//		return null;
+//	}
+//
+//}
+//void loadFaceDb(String dbPath) {
+//	try {
+//		BufferedReader reader = new BufferedReader(new FileReader(dbPath));
+//		String line;
+//		while ((line = reader.readLine()) != null) {
+//			String[] values = line.split(",");
+//			String name = values[0];
+//			//float[] embedding = Arrays.stream(values[1].split(" ")).map(Float::parseFloat).toArray(float[]::new);
+//			//faceDb.put(name, embedding);
+//		}
+//		reader.close();
+//	} catch (IOException e) {
+//		e.printStackTrace();
+//	}
+//}
+//void recognizeFaces(Mat frame) {
+////	Mat gray = new Mat();
+////	Imgproc.cvtColor(frame, gray, Imgproc.COLOR_BGR2GRAY);
+////	MatOfRect faces = new MatOfRect();
+////	cascadeClassifier.detectMultiScale(gray, faces, 1.3, 5);
+////
+////	for (Rect rect : faces.toArray()) {
+////		Mat face = new Mat(frame, rect);
+////		Imgproc.resize(face, face, new Size(160, 160));
+////		float[] embedding = faceNetModel.getFaceEmbedding(face);
+////		String name = recognizeFace(embedding);
+////		Imgproc.putText(frame, name, new Point(rect.x, rect.y - 10), Imgproc.FONT_HERSHEY_SIMPLEX, 1, new Scalar(0, 255, 0), 2);
+////		Imgproc.rectangle(frame, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(0, 255, 0), 2);
+////	}
+//}
+//
+//String recognizeFace(float[] embedding) {
+//	String name = "Unknown";
+//	double minDistance = Double.MAX_VALUE;
+//	for (Map.Entry<String, float[]> entry : faceDb.entrySet()) {
+//		float[] dbEmbedding = entry.getValue();
+//		double distance = calculateDistance(embedding, dbEmbedding);
+//		if (distance < minDistance) {
+//			minDistance = distance;
+//			name = entry.getKey();
+//		}
+//	}
+//	if (minDistance > threshold) {
+//		name = "Unknown";
+//	}
+//	return name;
+//}
+//
+//double calculateDistance(float[] embedding1, float[] embedding2) {
+//	double sum = 0.0;
+//	for (int i = 0; i < embedding1.length; i++) {
+//		sum += Math.pow(embedding1[i] - embedding2[i], 2);
+//	}
+//	return Math.sqrt(sum);
+//}
+
+
 Mat matrix =new Mat();
 VideoCapture capture = new VideoCapture(0);
 capture.open(0)
@@ -76,71 +198,111 @@ faceCascade.load(fileFromGit.getAbsolutePath());
 int absoluteFaceSize=0;
 Tab t =new Tab()
 boolean run = true
-
+VBox workingMemory = new VBox()
 ZooModel<Image, DetectedObjects> model  = PredictorFactory.imageContentsFactory(ImagePredictorType.ultranet);
 Predictor<Image, DetectedObjects> predictor =model.newPredictor()
-
-Image factoryFromImage=null
 factory=ImageFactory.getInstance()
+
+
 class UniquePerson{
 	String name=""
-	float[] features;
+	ArrayList<float[]> features=[];
 	String referenceImageLocation;
 	Image memory;
 	int timesSeen = 1;
+	long time=System.currentTimeMillis()
+	HBox box
 }
-ArrayList<UniquePerson> knownPeople =[]
-UniquePerson currentPerson=null
+HashMap<BufferedImage,org.opencv.core.Point> factoryFromImage=null
+HashMap<UniquePerson,org.opencv.core.Point> currentPersons=null
 
 new Thread({
-	try {
-		Predictor<Image, float[]> features = PredictorFactory.faceFeatureFactory()
-		JniUtils.setGraphExecutorOptimize(false);
-		while(!Thread.interrupted() && run) {
+
+	ArrayList<UniquePerson> knownPeople =[]
+	Predictor<Image, float[]> features = PredictorFactory.faceFeatureFactory()
+	JniUtils.setGraphExecutorOptimize(false);
+	float confidence=0.91
+	long timeout = 3000
+	long countPeople=1
+	while(!Thread.interrupted() && run) {
+		try {
 			if(factoryFromImage==null) {
 				Thread.sleep(16)
 				continue;
 			}
-			Image tmp=factoryFromImage
-			//println "Processing new image "
-			float[] id = features.predict(tmp);
-			boolean found=false;
-			def duplicates =[]
-			for(UniquePerson p:knownPeople) {
-				float result = PredictorFactory.calculSimilarFaceFeature(id, p.features)
-				if (result>0.85) {
-					if(found) {
-						duplicates.add(p)
-					}else {
+			HashMap<BufferedImage,org.opencv.core.Point> local = new HashMap<>()
+			local.putAll(factoryFromImage)
+			HashMap<UniquePerson,org.opencv.core.Point> tmpPersons = new HashMap<>()
+			for(BufferedImage imgBuff:local.keySet()) {
+				ai.djl.modality.cv.Image cmp= factory.fromImage(imgBuff)
+				def point = local.get(imgBuff);
+				//println "Processing new image "
+				float[] id = features.predict(cmp);
+				boolean found=false;
+				def duplicates =[]
+
+				for(UniquePerson p:knownPeople) {
+					int count= 0;
+					for(int i=0;i<p.features.size();i++) {
+						float[] featureFloats =p.features.get(i);
+						float result = PredictorFactory.calculSimilarFaceFeature(id, featureFloats)
+						if (result>confidence) {
+							if(found) {
+								duplicates.add(p)
+							}else {
+								count++;
+								tmpPersons.put(p, point)
+								p.timesSeen++
+								p.time=System.currentTimeMillis()
+								if(result<(confidence+0.02))
+									if(p.features.size()<21) {
+										p.features.add(id)
+									}
+							}
+						}
+					}
+					if(count>(p.features.size()/3)) {
 						found=true;
-						currentPerson=p;
-						p.timesSeen++
-						println "Seeing "+p.name+" "+p.timesSeen+" times confidence:"+result
+						break;
 					}
 				}
+				for(int i=0;i<knownPeople.size();i++) {
+					UniquePerson p = knownPeople.get(i)
+					if((System.currentTimeMillis()-p.time)>timeout) {
+						duplicates.add(p)
+					}
+				}
+				for(UniquePerson p:duplicates) {
+					knownPeople.remove(p)
+					BowlerStudio.runLater({workingMemory.getChildren().remove(p.box)})
+					println "Removing "+p.name
+				}
+
+				if(found==false) {
+					UniquePerson p = new UniquePerson();
+					p.features.add(id);
+					p.name="Person "+(countPeople++)
+					String tmpDirsLocation = System.getProperty("java.io.tmpdir")+"/idFiles/"+p.name+".jpeg";
+					p.box= new HBox()
+					WritableImage tmpImg = SwingFXUtils.toFXImage(imgBuff, null);
+					p.box.getChildren().addAll(new ImageView(tmpImg))
+					p.box.getChildren().addAll(new Label(p.name))
+
+					BowlerStudio.runLater({workingMemory.getChildren().add(p.box)})
+
+
+					p.referenceImageLocation=tmpDirsLocation
+					println "New person found! "+tmpDirsLocation
+					knownPeople.add(p)
+				}
 			}
-			for(def d:duplicates) {
-				knownPeople.remove(d)
-			}
-			if(found==false) {
-				UniquePerson p = new UniquePerson();
-				p.features=id;
-				p.name="Person "+knownPeople.size()+1
-				String tmpDirsLocation = System.getProperty("java.io.tmpdir")+"/idFiles/"+p.name+".jpeg";
-				
-				//File local = new File(tmpDirsLocation)
-				//local.getParentFile().mkdirs();
-				//local.createNewFile();
-				//tmp.save(new FileOutputStream(local), "jpeg")
-				p.referenceImageLocation=tmpDirsLocation
-				println "New person found! "+tmpDirsLocation
-				knownPeople.add(p)
-			}
+			currentPersons=tmpPersons
+		}catch(Throwable tr) {
+			BowlerStudio.printStackTrace(tr)
+			//run=false;
 		}
-	}catch(Throwable tr) {
-		BowlerStudio.printStackTrace(tr)
-		run=false;	
 	}
+
 }).start()
 
 while(!Thread.interrupted() && run) {
@@ -173,29 +335,49 @@ while(!Thread.interrupted() && run) {
 
 				//println "Capture success"
 				// Creating BuffredImage from the matrix
-				BufferedImage image = new BufferedImage(matrix.width(),
+				BufferedImage imageFull = new BufferedImage(matrix.width(),
 						matrix.height(), BufferedImage.TYPE_3BYTE_BGR);
 
-				WritableRaster raster = image.getRaster();
-				DataBufferByte dataBuffer = (DataBufferByte) raster.getDataBuffer();
-				byte[] data = dataBuffer.getData();
-				matrix.get(0, 0, data);
+				WritableRaster r = imageFull.getRaster();
+				DataBufferByte db = (DataBufferByte) r.getDataBuffer();
+				byte[] dataFull = db.getData();
+				matrix.get(0, 0, dataFull);
+				ai.djl.modality.cv.Image tmp= factory.fromImage(imageFull)
 
-				def tmp= factory.fromImage(image)
+
 				DetectedObjects detection = predictor.predict(tmp);
 				List<DetectedObject> items = detection.items();
 				Rect[] facesArray = new Rect[items.size()];
-				if( items.size()==0) {
-					factoryFromImage=null
-					
-				}else {
-					factoryFromImage=tmp
-				}
+				HashMap<BufferedImage,org.opencv.core.Point> facePlaces = new HashMap<>()
+
 				for (int detectionIndex = 0; detectionIndex < items.size(); detectionIndex++) {
+
 					DetectedObject c = items.get(detectionIndex);
 					BoundingBox cGetBoundingBox = c.getBoundingBox();
-					def topLeft = cGetBoundingBox.getPoint();
-					def rect = cGetBoundingBox.getBounds();
+					ai.djl.modality.cv.output.Point topLeft = cGetBoundingBox.getPoint();
+					ai.djl.modality.cv.output.Rectangle rect = cGetBoundingBox.getBounds();
+					facesArray[detectionIndex]=new Rect(topLeft.getX()*matrix.width(),topLeft.getY()*matrix.height(),rect.getWidth()*matrix.width() ,rect.getHeight()*matrix.height())
+
+					Rect crop =facesArray[detectionIndex]
+					try {
+						Mat image_roi = new Mat(matrix,crop);
+						BufferedImage image = new BufferedImage((int)rect.getWidth()*matrix.width() ,(int)rect.getHeight()*matrix.height(), BufferedImage.TYPE_3BYTE_BGR);
+						WritableRaster raster = image.getRaster();
+						DataBufferByte dataBuffer = (DataBufferByte) raster.getDataBuffer();
+						byte[] data = dataBuffer.getData();
+						image_roi.get(0,0, data);
+
+
+						nameLoc = new Point(topLeft.getX()*matrix.width(),topLeft.getY()*matrix.height()-5)
+						facePlaces.put(image, nameLoc)
+					}catch(Throwable tr) {
+						BowlerStudio.printStackTrace(tr)
+						continue;
+					}
+					// draw the face in the corner
+					//matrix.get(0, 0, data);
+
+
 					Iterator<ai.djl.modality.cv.output.Point> path = cGetBoundingBox.getPath().iterator();
 					ArrayList<ai.djl.modality.cv.output.Point> list = new ArrayList<>();
 					// sort into an ordered list
@@ -228,12 +410,12 @@ while(!Thread.interrupted() && run) {
 							// angle is 0, they are the same
 						}
 					}
+
 					//lm.get
-					facesArray[detectionIndex]=new Rect(topLeft.getX()*matrix.width(),topLeft.getY()*matrix.height(),rect.getWidth()*matrix.width() ,rect.getHeight()*matrix.height())
 					//System.out.println(c);
 					//System.out.println("Name: "+c.getClassName() +" probability "+c.getProbability()+" center x "+topLeft.getX()+" center y "+topLeft.getY()+" rect h"+rect.getHeight()+" rect w"+rect.getWidth() );
 					Imgproc.rectangle(matrix, facesArray[detectionIndex].tl(), facesArray[detectionIndex].br(), new Scalar(0, 255, 0), 3);
-					Imgproc.putText(matrix, c.getClassName(), new Point(topLeft.getX()*matrix.width(),topLeft.getY()*matrix.height()-5), 3,1,  new Scalar(0, 255, 0));
+
 					if(list.size()>3) {
 						for(int j=0;j<2;j++) {
 							ai.djl.modality.cv.output.Point p= list.get(j)
@@ -247,18 +429,36 @@ while(!Thread.interrupted() && run) {
 						}
 					}
 				}
-				matrix.get(0, 0, data);
+				if( items.size()==0) {
+					factoryFromImage=null
+
+				}else {
+					factoryFromImage=facePlaces
+				}
+				if(currentPersons!=null) {
+					HashMap<UniquePerson,org.opencv.core.Point> lhm = new HashMap<>()
+					lhm.putAll(currentPersons)
+					for(UniquePerson currentPerson:lhm.keySet()) {
+						def p = lhm.get(currentPerson)
+						Imgproc.putText(matrix, currentPerson.name,p , 3,1,  new Scalar(0, 255, 0));
+					}
+				}
+				// Write Matrix wiht image, and detections ovelaid, onto the matrix
+				matrix.get(0, 0, dataFull);
 				//println detection
 
 				// Creating the Writable Image
 				if(img==null) {
-					img = SwingFXUtils.toFXImage(image, null);
+					img = SwingFXUtils.toFXImage(imageFull, null);
 
 					t=new Tab("Imace capture ");
-					t.setContent(new ImageView(img))
+					HBox content = new HBox()
+					content.getChildren().add(new ImageView(img))
+					content.getChildren().add(workingMemory)
+					t.setContent(content)
 					BowlerStudioController.addObject(t, null);
 				}else{
-					SwingFXUtils.toFXImage(image, img);
+					SwingFXUtils.toFXImage(imageFull, img);
 				}
 			}
 		}else {
@@ -266,7 +466,7 @@ while(!Thread.interrupted() && run) {
 
 			throw new RuntimeException("Camera failed!");
 		}
-	}catch(Error tr) {
+	}catch(Throwable tr) {
 		BowlerStudio.printStackTrace(tr)
 		run=false;
 		break;
@@ -274,6 +474,7 @@ while(!Thread.interrupted() && run) {
 
 }
 run=false;
+
 BowlerStudioController.removeObject(t)
 capture.release()
 println "clean exit and closed camera"
