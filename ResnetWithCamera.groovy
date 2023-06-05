df59559bf53f24863fe320dd417a2d495c88bd60
Kevin Harrington <mad.hephaestus@gmail.com>
2023-06-05 15:42:52 -0400

    remove opencv HAAR cascade loading

--- ResnetWithCamera.groovy
+++ ResnetWithCamera.groovy
@@ -59,120 +59,7 @@ import org.opencv.objdetect.CascadeClassifier;
 import org.opencv.objdetect.Objdetect;
 import org.opencv.videoio.VideoCapture;
 import com.neuronrobotics.bowlerstudio.opencv.OpenCVManager;
-//import org.tensorflow.Graph;
-//import org.tensorflow.Session;
-//import org.tensorflow.Tensor;
-//import org.tensorflow.TensorFlow;
 
-
-// For proper execution of native libraries
-// Core.NATIVE_LIBRARY_NAME must be loaded before
-// calling any of the opencv methods
-
-// Code from https://www.javatpoint.com/real-time-face-recognition-in-java
-//Map<String, float[]> faceDb = new HashMap<>();
-//FaceNetModel faceNetModel = new FaceNetModel("facenet.pb");
-//class FaceNetModel {
-//
-//	private Graph graph;
-//	private Session session;
-//	private String modelPath;
-//
-//	public FaceNetModel(String modelPath) {
-//		this.modelPath = modelPath;
-//	}
-//
-//	public void loadModel() {
-//		graph = new Graph();
-//		byte[] modelBytes = readAllBytesOrExit(Paths.get(modelPath));
-//		graph.importGraphDef(modelBytes);
-//		session = new Session(graph);
-//	}
-//
-//	public float[] getFaceEmbedding(Mat face) {
-//		float[] embedding = null;
-//		try {
-//			Tensor<Float> tensor = normalizeImage(face)
-//			Tensor<Float> output = session.runner()
-//			.feed("input_1", tensor)
-//			.fetch("Bottleneck_BatchNorm/batchnorm/add_1")
-//			.run()
-//			.get(0)
-//			.expect(Float.class);
-//			embedding = new float[(int) output.shape()[1]];
-//			output.copyTo(embedding);
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//		return embedding;
-//	}
-//
-//	private Tensor<Float> normalizeImage(Mat mat) {
-//		Imgproc.cvtColor(mat, mat, Imgproc.COLOR_BGR2RGB);
-//		mat.convertTo(mat, CvType.CV_32F);
-//		Core.divide(mat, Scalar.all(255.0f), mat);
-//		return Tensor.create(mat.reshape(1, 160, 160, 3));
-//	}
-//
-//	private static byte[] readAllBytesOrExit(Path path) {
-//		try {
-//			return Files.readAllBytes(path);
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//			System.exit(-1);
-//		}
-//		return null;
-//	}
-//
-//}
-//void loadFaceDb(String dbPath) {
-//	try {
-//		BufferedReader reader = new BufferedReader(new FileReader(dbPath));
-//		String line;
-//		while ((line = reader.readLine()) != null) {
-//			String[] values = line.split(",");
-//			String name = values[0];
-//			//float[] embedding = Arrays.stream(values[1].split(" ")).map(Float::parseFloat).toArray(float[]::new);
-//			//faceDb.put(name, embedding);
-//		}
-//		reader.close();
-//	} catch (IOException e) {
-//		e.printStackTrace();
-//	}
-//}
-//void recognizeFaces(Mat frame) {
-////	Mat gray = new Mat();
-////	Imgproc.cvtColor(frame, gray, Imgproc.COLOR_BGR2GRAY);
-////	MatOfRect faces = new MatOfRect();
-////	cascadeClassifier.detectMultiScale(gray, faces, 1.3, 5);
-////
-////	for (Rect rect : faces.toArray()) {
-////		Mat face = new Mat(frame, rect);
-////		Imgproc.resize(face, face, new Size(160, 160));
-////		float[] embedding = faceNetModel.getFaceEmbedding(face);
-////		String name = recognizeFace(embedding);
-////		Imgproc.putText(frame, name, new Point(rect.x, rect.y - 10), Imgproc.FONT_HERSHEY_SIMPLEX, 1, new Scalar(0, 255, 0), 2);
-////		Imgproc.rectangle(frame, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(0, 255, 0), 2);
-////	}
-//}
-//
-//String recognizeFace(float[] embedding) {
-//	String name = "Unknown";
-//	double minDistance = Double.MAX_VALUE;
-//	for (Map.Entry<String, float[]> entry : faceDb.entrySet()) {
-//		float[] dbEmbedding = entry.getValue();
-//		double distance = calculateDistance(embedding, dbEmbedding);
-//		if (distance < minDistance) {
-//			minDistance = distance;
-//			name = entry.getKey();
-//		}
-//	}
-//	if (minDistance > threshold) {
-//		name = "Unknown";
-//	}
-//	return name;
-//}
-//
 double calculateDistance(float[] embedding1, float[] embedding2) {
 	double sum = 0.0;
 	for (int i = 0; i < embedding1.length; i++) {
@@ -201,15 +88,11 @@ VideoCapture capture = OpenCVManager.get(0).getCapture()
 Mat matrix =new Mat();
 WritableImage img = null;
 CascadeClassifier faceCascade = new CascadeClassifier();
-//File fileFromGit = ScriptingEngine.fileFromGit("https://github.com/CommonWealthRobotics/harr-cascade-archive.git", "resources/haarcascades/haarcascade_frontalcatface_extended.xml")
-File fileFromGit = ScriptingEngine.fileFromGit("https://github.com/CommonWealthRobotics/harr-cascade-archive.git", "resources/haarcascades/haarcascade_frontalface_default.xml")
 
-faceCascade.load(fileFromGit.getAbsolutePath());
 int absoluteFaceSize=0;
 Tab t =new Tab()
 boolean run = true
 VBox workingMemory = new VBox()
-//ZooModel<Image, DetectedObjects> model  = PredictorFactory.imageContentsFactory(ImagePredictorType.ultranet);
 Predictor<Image, DetectedObjects> predictor =PredictorFactory.imageContentsFactory(ImagePredictorType.ultranet)
 factory=ImageFactory.getInstance()
 
